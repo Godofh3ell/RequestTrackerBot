8c15093da7e206ba341cbb172c03fb693c22a6a0
Godofh3ell <66544811+Godofh3ell@users.noreply.github.com>
2022-01-21 20:13:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -292,7 +292,7 @@ async def requestHandler(bot:Update, msg:Message):
                 )
             )
 
-            replyText = f"<b>👋 Hello {mentionUser} !!\n\n📍 Your Request for {contentRequested} has been submitted to the admins.\n\n🚀 Your Request Will Be Uploaded soon.\n\n 📌 Note :- If You Didn't Follow The Request Format Your Request Will be Rejected \n\n 📄 See Request Format by Sending #format \n\n📌 Please Note that Admins might be busy. So, this may take more time.\n\n 🔖Join @tsnmstream4u & @only_4K to Get Updates \n\n👇 See Your Request Status Here 👇</b>"
+            replyText = f"<b>👋 Hello {mentionUser} !!\n\n📍 Your Request for {contentRequested} has been submitted to the admins.\n\n🚀 Your Request Will Be Uploaded soon.\n\n 📌 Note :- If You Didn't Follow The Request Format Your Request Will be Rejected \n\n 📄 See Request Format by Sending #format \n\n📌 Please Note that Admins might be busy. So, this may take more time.\n\n 🔖Join @tsnmstream4u & @only_4K to Get Updates \n\n👇 See Your Request Status Here </b>"
 
             # Sending message for user in group
             await msg.reply_text(
@@ -304,7 +304,7 @@ async def requestHandler(bot:Update, msg:Message):
                         [
                             InlineKeyboardButton(
                                 "⏳Request Status⏳",
-                                url = f"https://t.me/c/{channelIDPro}/{requestMSG.message_id}"
+                                url = f"https://t.me/TSNM_INDEX/{requestMSG.message_id}"
                             )
                         ]
                     ]
